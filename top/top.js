6b65f5dea99ef097b507aaba9014b7249c3962f9
1753445107 <1753445107@qq.com>
2016-10-29 17:18:42 +0800

    豆瓣

---
+++ top/top.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alice on 16/10/23.
+ */
+(function () {
+    var topModule = angular.module('doubanApp.topModule',[]);
+    topModule.controller('TopCtrl',['$scope',function ($scope) {
+
+    }]);
+})();
